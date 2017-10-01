fb10485e073b60ba38902a997608552a086cccad
Mike Nadasy <mikenadasy@gmail.com>
2017-10-01 16:02:14 -0400

    add delete buttons done

--- src/components/addReport.vue
+++ src/components/addReport.vue
@@ -4,26 +4,59 @@
         <form>
             <div class="form-group">
                 <label for="title">Report Title:</label>
-                <input id="title" type="text" class="form-control" v-model="report.title" required>
+                <input id="title" type="text" class="form-control" v-model.lazy="report.title" required>
             </div>
             <div class="form-group">
                 <label for="accomplishments">Accomplishments:</label>
-                <input id="accomplishments" type="text" class="form-control" required>
+                <div class="input-group">
+                    <input id="accomplishments" type="text" class="form-control" v-model="report.accomplishment" required>
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary btn-sm" v-on:click="addItem('accomplishment')">+</button>
+                    </span>
+                </div>
             </div>
             <div class="form-group">
                 <label for="risks">Risks:</label>
-                <input id="risks" type="text" class="form-control" required>
+                <div class="input-group">
+                    <input id="risks" type="text" class="form-control" v-model="report.risk" required>
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary btn-sm" v-on:click="addItem('risk')">+</button>
+                    </span>
+                </div>
             </div>
             <div class="form-group">
                 <label for="issues">Issues:</label>
-                <input id="issues" type="text" class="form-control" required>
+                <div class="input-group">
+                    <input id="issues" type="text" class="form-control" v-model="report.issue" required>
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary btn-sm" v-on:click="addItem('issue')">+</button>
+                    </span>
+                </div>
+
             </div>
         </form>
         <div id="preview">
             <h3 class="text-muted">Preview Report</h3>
-            <p class="lead">Title: {{report.title}} </p>
+            <p class="h6">Title:
+                <span class="text-muted">{{report.title}}</span>
+            </p>
+            <p class="h6">Accomplishments</p>
             <ul>
-                <li v-for="(item, index) in report.accomplishments">{{item.item}} <button class="btn btn-danger btn-sm" v-on:click="removeItem(index)">X</button></li>
+                <li v-for="(item, index) in report.accomplishments" class="text-muted">{{item.item}}
+                    <button class="btn btn-danger btn-sm" v-on:click="removeItem(index, 'accomplishment')">X</button>
+                </li>
+            </ul>
+            <p class="h6">Risks</p>
+            <ul>
+                <li v-for="(item, index) in report.risks" class="text-muted">{{item.item}}
+                    <button class="btn btn-danger btn-sm" v-on:click="removeItem(index, 'risk')">X</button>
+                </li>
+            </ul>
+             <p class="h6">Issues</p>
+            <ul>
+                <li v-for="(item, index) in report.issues" class="text-muted">{{item.item}}
+                    <button class="btn btn-danger btn-sm" v-on:click="removeItem(index, 'issue')">X</button>
+                </li>
             </ul>
         </div>
     </div>
@@ -40,15 +73,46 @@ export default {
                     { item: "consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt" },
                     { item: "Ut ninja wisi enim ad minim veniam, quis nostrud" },
                     { item: "Duis ninja autem vel eum iriure dolor in hendrerit" }
-                ]
+                ],
+                accomplishment: "",
+                risks: [
+                    { item: "risk one" },
+                    { item: "risk two" },
+                    { item: "risk threee" },
+                    { item: "risk four" }
+
+                ],
+                risk: "",
+                issues: [
+                    { item: "issue one" },
+                    { item: "issue two" },
+                    { item: "issue threee" },
+                    { item: "issue four" }
+                ],
+                issue: ""
             }
 
         }
     },
-    methods:{
-        removeItem: function(index){
-            console.log(index);
-            this.report.accomplishments.splice(index,1);            
+    methods: {
+        removeItem: function(index, type) {
+            if (type === 'accomplishment') {
+                this.report.accomplishments.splice(index, 1);
+            } else if (type === 'risk') {
+                this.report.risks.splice(index, 1);
+            } else if (type === 'issue') {
+                this.report.issues.splice(index, 1);
+            }            
+        },
+        addItem: function(type) {
+            if (type === 'accomplishment') {
+                this.report.accomplishments.push({ item: this.report.accomplishment });
+            } else if (type === 'risk') {
+                this.report.risks.push({ item: this.report.risk });
+            } else if (type === 'issue') {
+                this.report.issues.push({item: this.report.issue});
+            }
+
         }
     }
 }
